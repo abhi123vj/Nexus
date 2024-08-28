c5e643dcb6c2d766f9199648169a6032d16fc7f8
4bh16am <45392298+abhi123vj@users.noreply.github.com>
2024-08-28 14:51:11 +0530

    welcome added

--- client/src/pages/auth/index.tsx
+++ client/src/pages/auth/index.tsx
@@ -1,8 +1,18 @@
-
 const Auth = () => {
   return (
-    <div>Auth</div>
-  )
-}
+    <div className="h-[100vh] w-[100vw] flex items-center justify-center">
+      <div className="h-[80vh] w-[80vw] bg-white border-2 border-white text-opacity-90 shadow-2xl md:w-[80vw] lg:w-[70vw] xl:w-[60vw] rounded-3xl grid xl:grid-cols-2"></div>
+        <div className="flex flex-col gap-10 items-center justify-center">
+            <div className="flex items-center justify-center flex-col">
+                <div className="flex items-center justify-center">
+                    <h1 className="text-5xl font-bold md:text-6xl"> Welcome</h1>
 
-export default Auth
+                </div>
+            </div>
+
+        </div>
+    </div>
+  );
+};
+
+export default Auth;
